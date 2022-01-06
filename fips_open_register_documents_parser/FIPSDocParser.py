90d780b2d553922795b0da363e63e8f066055d86
Victoria Zelepukhina <ikkalapse@gmail.com>
2022-01-06 11:26:22 +0400

    EVM, DB, TIMS parsers added and tested

--- fips_open_register_documents_parser/FIPSDocParser.py
+++ fips_open_register_documents_parser/FIPSDocParser.py
@@ -3,70 +3,80 @@ from bs4 import BeautifulSoup
 import re
 
 
-class FIPSDOCParser:
+class FIPSDocParser:
 
-    def __init__(self, html_data, **kwargs):
-        self.html_data = html_data
-        self.soup = BeautifulSoup(self.html_data, 'html.parser')
-        self.main = self.soup.find('div', {'id': 'main'})
-        self.bib = self.soup.find('table', {'id': 'bib'})
-        self.parsed = {'title': None,
-                       'authors': None,
-                       'holders': None,
-                       'reg_number': None,
-                       'reg_date': None,
-                       'app_number': None,
-                       'app_date': None}
+    def __init__(self):
+        self.__html_data = None
+        self.__soup = None
+        self.__main = None
+        self.__bib = None
+        self.parsed = None
+
+    @property
+    def soup(self):
+        return self.__soup
+
+    @property
+    def main(self):
+        return self.__main
+
+    @property
+    def bib(self):
+        return self.__bib
+
+    @property
+    def html_data(self):
+        return self.__html_data
+
+    @html_data.setter
+    def html_data(self, value):
+        self.__html_data = str(value)
+        self.__soup = BeautifulSoup(self.__html_data, 'html.parser')
+        self.__main = self.soup.find('div', {'id': 'main'})
+        self.__bib = self.soup.find('table', {'id': 'bib'})
+        self.parsed = {}
 
     def parse(self):
         raise NotImplementedError()
 
 
-class FIPSDocEVMparser(FIPSDOCParser):
+class FIPSDocEVMDBParser(FIPSDocParser):
 
     def parse(self):
-        self.parsed['registration number'] = self.find_regnumber(self.bib)  # registration number
-        self.parsed['registration date'] = self.find_regdate(self.bib)  # registration date
-        self.parsed['publication date'] = self.find_pubdate(self.bib)  # publication date
-        self.parsed['application data'] = self.find_application(self.bib)  # application
-        self.parsed['application number'] = self.parsed['application data'][0]  # application number
-        self.parsed['application date'] = self.parsed['application data'][1]  # application date
-        self.parsed['authors'] = self.find_authors(self.bib)  # authors
-        self.parsed['right holders'] = self.find_right_holders(self.bib)  # right holders
+        self.parsed['reg_number'] = self.find_regnumber(self.bib)  # registration number
+        self.parsed['reg_date'] = self.find_regdate(self.bib)  # registration date
+        self.parsed['pub_date'] = self.find_pubdate(self.bib)  # publication date
+        app_data = self.find_application(self.bib)  # application data
+        self.parsed['app_number'] = app_data[0]  # application number
+        self.parsed['app_date'] = app_data[1]  # application date
         self.parsed['title'] = self.find_title(self.main)  # title
+        self.parsed['title_new'] = self.find_title_new(self.soup)  # title new
+        self.parsed['authors'] = self.find_authors(self.bib)  # authors
+        self.parsed['holders'] = self.find_right_holders(self.bib)  # right holders
+        self.parsed['holders_new'] = self.find_right_holders_new(self.soup)  # right holders new
         self.parsed['abstract'] = self.find_abstract(self.main)  # abstract
-        self.parsed['tool'], self.parsed['size'], self.parsed['pc'], self.parsed['os'], self.parsed['abstract'] = \
-            self.find_extra(self.main)  # extra data
+        self.parsed['tool'] = self.find_tool(self.main.text) if self.main is not None else ''
+        self.parsed['size'] = self.find_size(self.main.text) if self.main is not None else ''
 
-    def find_extra(self, main):
-        titabs = main.find_all('p', {'class': 'TitAbs'})
-        tool = '' # tool
-        size = '' # size
-        pc = '' # pc
-        os = '' # operation system
-        abstract = '' # abstract
-        if len(titabs) == 6:
-            tool = self.find_tool_db(main)
-            size = self.find_size_db(main)
-            pc = self.find_pc_db(main)
-            os = self.find_os_db(main)
-        else:
-            tool = self.find_tool_evm(main)
-            size = self.find_size_evm(main)
-            pc = self.find_pc_evm(main, abstract)
-            os = self.find_os_evm(main, abstract)
-            abstract = self.clear_abstract_evm(abstract)
-        return tool, size, pc, os, abstract
+    @staticmethod
+    def find_tool(content):
+        return ''
 
-    def find_regnumber(self, content):
-        """Finds a registration number in the content."""
+    @staticmethod
+    def find_size(content):
+        return ''
+
+    @staticmethod
+    def find_regnumber(content):
+        """Номер регистрации."""
 
         if content is not None:
             return content.find('a', {'title': 'Ссылка на реестр (открывается в отдельном окне)'}).text
         return ''
 
-    def find_regdate(self, content):
-        """Finds a registration date in the content."""
+    @staticmethod
+    def find_regdate(content):
+        """Дата регистрации."""
 
         try:
             tmp = content.find_all('td')[0].find_all('p')[1].find('b').text
@@ -74,8 +84,9 @@ class FIPSDocEVMparser(FIPSDOCParser):
         except (IndexError, AttributeError):
             return ''
 
-    def find_pubdate(self, content):
-        """Finds a publication date in the content."""
+    @staticmethod
+    def find_pubdate(content):
+        """Дата публикации."""
 
         try:
             tmp = content.find_all('td')[0].find_all('p')[3].find('b').text
@@ -83,132 +94,140 @@ class FIPSDocEVMparser(FIPSDOCParser):
         except (IndexError, AttributeError):
             return ''
 
-    def find_application(self, content):
-        """Finds an application date and application number in the content."""
+    @staticmethod
+    def find_application(content):
+        """Данные заявки."""
 
-        appdata_pattern = re.compile(r'([0-9]{5,})\s+([0-9]{2}\.[0-9]{2}\.[0-9]{4})', flags=re.M)
-        appdata = appdata_pattern.findall(content.text)
         res = ['', '']
-        if (len(appdata) == 1):
-            res = list(appdata[0])
+        try:
+            appdata_pattern = re.compile(r'([0-9]{5,})\s+([0-9]{2}\.[0-9]{2}\.[0-9]{4})', flags=re.M)
+            appdata = appdata_pattern.findall(content.text)
+            if len(appdata) == 1:
+                res = list(appdata[0])
+        except (IndexError, AttributeError):
+            pass
         return res
 
-    def find_authors(self, content):
-        """Finds authors in the content."""
+    @staticmethod
+    def find_authors(content):
+        """Авторы."""
 
         try:
-            tmp = content.find('td', {'id': 'bibl'}) \
-                .find_all('p')[0].find('b').text
+            tmp = content.find('td', {'id': 'bibl'}).find_all('p')
+            return str(tmp[0].find('b').extract()).replace('<b>', '').replace('</b>', '') \
+                if len(tmp) == 2 else ''
         except (IndexError, AttributeError):
-            tmp = ''
-        return tmp
-
-    def find_right_holders(self, content):
-        """Finds right holders in the content."""
-
-        try:
-            tmp = content.find('td', {'id': 'bibl'}) \
-                .find_all('p')[1].find('b').text \
-                .replace('\n\t\t\t\t\t\t\t\t\t', '')
-            return tmp
-        except IndexError:
             return ''
 
-    def find_title(self, content):
-        """Finds a title in the content."""
+    @staticmethod
+    def find_right_holders(content):
+        """Правообладатели."""
+
+        try:
+            tmp = content.find('td', {'id': 'bibl'}).find_all('p')
+            holders = str(tmp[1].find('b').extract()).replace('<b>', '').replace('</b>', '') \
+                if len(tmp) == 2 else str(tmp[0].find('b').extract()).replace('<b>', '').replace('</b>', '')
+            return holders.replace('\n\t\t\t\t\t\t\t\t\t', '')
+        except (IndexError, AttributeError):
+            return ''
+
+    @staticmethod
+    def find_right_holders_new(content):
+        """Правообладатели, которые указаны в извещении (последние данные)."""
+
+        try:
+            # Учитываем вхождение слова "правообладатель" в любых вариантах
+            data = content.find_all('p', {'class': 'izv'},
+                                   text=re.compile(".*правообладатель.*",
+                                                   flags=re.IGNORECASE))[-1]
+            data = data.next_element.next_element.next_element
+            if re.match(r'следует\s+читать', data.get_text(), re.IGNORECASE) is not None:
+                data = data.next_element.next_element.next_element
+            return data.get_text()
+        except (IndexError, AttributeError):
+            return ''
+
+    @staticmethod
+    def find_title(content):
+        """Название ОИС."""
 
         try:
             tmp = content.find_all('p', {'class': 'TitAbs'})[0] \
                 .find('b').text
             return tmp
-        except IndexError:
+        except (IndexError, AttributeError):
             return ''
 
-    def find_abstract(self, content):
-        """Finds an abstract in the content."""
+    @staticmethod
+    def find_title_new(content):
+        """Новое название ОИС, которое указано в извещении (последние данные)."""
+
+        try:
+            # Учитываем вхождение слова "правообладатель" в любых вариантах
+            data = content.find_all('p', {'class': 'izv'},
+                                   text=re.compile(".*название (базы данных|программы для ).*",
+                                                   flags=re.IGNORECASE))[-1]
+            data = data.next_element.next_element.next_element
+            if re.match(r'следует\s+читать', data.get_text(), re.IGNORECASE) is not None:
+                data = data.next_element.next_element.next_element
+            return data.get_text()
+        except (IndexError, AttributeError):
+            return ''
+
+    # <p class="izv">Изменения в поле: Название базы данных:</p>
+
+    @staticmethod
+    def find_abstract(content):
+        """Находит реферат."""
 
         try:
             tmp = content.find_all('p', {'class': 'TitAbs'})[1] \
                 .text.replace('\nРеферат:\n', '')
             return tmp
-        except IndexError:
+        except (IndexError, AttributeError):
             return ''
 
-    def find_pc_db(self, content):
-        """Finds a PC information for a database in the content."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[2] \
-                .text.replace('\nТип реализующей ЭВМ: ', '')
-            return tmp
-        except IndexError:
-            return ''
+class FIPSDocEVMParser(FIPSDocEVMDBParser):
+    """Парсер свидетельства о регистрации ПрЭВМ."""
 
-    def find_tool_db(self, content):
-        """Finds a DB Tool information in the content."""
+    @staticmethod
+    def find_tool(content):
+        """Язык программирования."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[3] \
-                .text.replace('\nВид и версия системы управления базой данных: ', '')
-            return tmp
-        except IndexError:
-            return ''
+        pattern = re.compile(r'Язык программирования:[\s\n\t]*([^\t].+)$', flags=re.M)
+        data = pattern.findall(content)
+        return data[0] if len(data) == 1 else ''
 
-    def find_os_db(self, content):
-        """Finds an OS information for a database in the content."""
+    @staticmethod
+    def find_size(content):
+        """Объём ПрЭВМ."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[4] \
-                .text.replace('\nВид и версия операционной системы: ', '')
-            return tmp
-        except IndexError:
-            return ''
+        pattern = re.compile(r'Объем программы для ЭВМ:[\s\n\t]*([^\t].+)$', flags=re.M)
+        data = pattern.findall(content)
+        return data[0] if len(data) == 1 else ''
 
-    def find_size_db(self, content):
-        """Finds a volume information for a database in the content."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[5] \
-                .text.replace('\n\n\t\t\t\t\t\t\tОбъем базы данных:\n\t\t\t\t\t\t', '')
-            return tmp
-        except IndexError:
-            return ''
+class FIPSDocDBParser(FIPSDocEVMDBParser):
+    """Парсер свидетельства о регистрации БД."""
 
-    def find_tool_evm(self, content):
-        """Finds a language information in the content."""
+    @staticmethod
+    def find_tool(content):
+        """Вид и версия СУБД."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[2] \
-                .text.replace('\nЯзык программирования: ', '')
-            return tmp
-        except IndexError:
-            return ''
+        pattern = re.compile(r'Вид и версия системы управления базой данных:\s+(.+)$', flags=re.M)
+        tool = pattern.findall(content)
+        return tool[0] if len(tool) == 1 else ''
 
-    def find_size_evm(self, content):
-        """Finds a volume information for a computer program in the content."""
+    @staticmethod
+    def find_size(content):
+        """Размер БД."""
 
-        try:
-            tmp = content.find_all('p', {'class': 'TitAbs'})[3] \
-                .text.replace('\n\n\t\t\t\t\t\t\tОбъем программы для ЭВМ:\n\t\t\t\t\t\t', '')
-            return tmp
-        except IndexError:
-            return ''
+        pattern = re.compile(r'Объем базы данных:\n?\t+([^\t].+)$', flags=re.M)
+        tool = pattern.findall(content)
+        return tool[0] if len(tool) == 1 else ''
 
-    def find_os_evm(self, content, abstract):
-        """Finds an OS information for a computer program in the content."""
 
-        os_pattern = re.compile(r'ОС:\s+(.+)\.$', flags=re.M)
-        os = os_pattern.findall(abstract)
-        return os[0] if len(os) == 1 else ''
-
-    def find_pc_evm(self, content, abstract):
-        """Finds a PC information for a computer program in the content."""
-
-        pc_pattern = re.compile(r'Тип ЭВМ:\s+(.+)\.\s*ОС:.+\.$', flags=re.M)
-        pc = pc_pattern.findall(abstract)
-        return pc[0] if len(pc) == 1 else ''
-
-    def clear_abstract_evm(self, abstract):
-        """Clear an abstract in the content."""
-
-        return re.sub(r"\s*Тип ЭВМ:\s+(.+)\.\s*ОС:.+\.$", "", abstract)
+class FIPSDocTIMSParser(FIPSDocEVMDBParser):
+    """Парсер свидетельства о регистрации ТИМС."""
+    pass
